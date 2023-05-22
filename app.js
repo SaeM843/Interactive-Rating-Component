cac9c398e2d5de7c000a56860e5478a332567606
Sae Matsuda <sae.matsuda.843@gmail.com>
2023-05-22 15:25:12 +0900

    added document selector

---
+++ app.js
@@ -0,0 +1,5 @@
+const rrating = document.querySelectorAll(".rating-circle");
+const submit_btn = document.querySelector(".btn");
+const selected_score = document.querySelector(".selected-score");
+const rating_state = document.querySelector(".rating_selector");
+const thanks_state = document.querySelector(".thanks-state");
