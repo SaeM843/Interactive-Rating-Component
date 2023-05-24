9913c0f485eef41e71437dd2ae22be8bbe7bdda1
Sae Matsuda <sae.matsuda.843@gmail.com>
2023-05-24 11:35:20 +0900

    updated functions

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const selected_score = document.querySelector(".selected-score");
 const rating_state = document.querySelector(".rating-state");
 const thanks_state = document.querySelector(".thanks-state");
 
-submit_btn.addEventListener("click", () => {
-    thanks_state.classList.remove("hidden")
-    rating_state.style.display = "none"
-
+submit_btn.addEventListener("click", e => {
+    e.preventDefault();
+    // get rating values //
+    const ratings = document.getElementsByName("score");
+    const selectedRating = [...ratings].filter(rating => rating.checked === true);
+    if (selectedRating.length === 0) return;
+    message(selectedRating[0].defaultValue);
+    showThankYou();
     })
 
+function message(rating) {
+    selected_score.textContent = rating;
+}
+
+function showThankYou() {
+    rating_state.style.display = "none";
+    thanks_state.style.display = "block";
+}
